6603aa4ce6b59b26204b1b2539c66c6d25cc839b
Theseregrets <rishitashaw49@gmail.com>
2021-07-08 20:27:21 +0530

    implemented

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
 function App() {
-  return <div className="App"></div>;
+  const onCreateOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "0.01",
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  };
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <PayPalButton
+          createOrder={(data, actions) => onCreateOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
