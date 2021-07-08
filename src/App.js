5e5f6b64343aa5fc55cc14a8da486781b15b2e1c
Theseregrets <rishitashaw49@gmail.com>
2021-07-08 20:18:04 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
 
 function App() {
   return <div className="App"></div>;
